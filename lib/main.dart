4f137bf4530cf4799f355ba980e0951c3b81fb92
Saubhagya Pandey <saubhagyapandey20@gmail.com>
2023-04-10 05:04:36 +0530

    updated work

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,15 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background_image.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+
         //TODO: Step 1 - Add background.png to this Container as a background image.
+
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
